c139c92c2a07e52106849f3d14dd7ae4b3823fa2
Ekaterina Kiseleva <kiselevakat94@bk.ru>
2022-09-22 21:21:03 +0300

    Added homework

---
+++ Homework_C#_2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.WriteLine("Введите трехзначное число: ");
+// int number = int.Parse(Console.ReadLine());
+// int num = number % 100;
+// Console.WriteLine($"Вторая цифра числа {number}: {num/10}");
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// if (number < 100)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// else
+// {
+//     while (number > 999) 
+//     {
+//         number = number / 10;
+
+//     }
+//     Console.WriteLine($"Третья цифра числа {number}: {number % 10}");
+// }
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// Console.WriteLine("Введите число, обозначающее день недели: ");
+// int number = int.Parse(Console.ReadLine());
+// if ((number >= 1) && (number <= 7))
+// {      
+//     if (number <= 5 && number >= 1)
+// {
+//     Console.WriteLine("День не является выходным");
+// }
+// else
+// {
+//     if ((number == 6) || (number == 7)) ;
+//     Console.WriteLine("Выходной!");
+// }
+// }
+// else
+// {
+//     Console.WriteLine("В неделе только 7 дней!");
+// }
